80c539c15fd45c8721fc1492a692aba236ea5134
unknown <uzairabid222@gmail.com>
2024-03-19 23:59:53 +0500

    changes

--- csvToExcel.py
+++ csvToExcel.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 try:
-    data = pd.read_csv('naver_copy_sample.csv', encoding='utf-8',encoding_errors='ignore')
+    data = pd.read_csv('list_06_Mar17.csv', encoding='utf-8',encoding_errors='ignore')
 except UnicodeDecodeError as e:
     print(f"Error reading CSV file: {e}")
     # Handle the exception as needed, e.g., log the error, skip problematic rows, etc.
     # You might want to add more specific handling based on your use case
 
 # Continue with the rest of your processing, assuming data is loaded successfully
-writer = pd.ExcelWriter('output.xlsx')
+writer = pd.ExcelWriter('list_06_Mar17.xlsx')
 data.to_excel(writer, index=False)
 writer.close()
 
