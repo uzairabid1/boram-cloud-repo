4830d5671f1ba548710e38db854662bc14e5e963
unknown <uzairabid222@gmail.com>
2024-03-20 21:44:45 +0500

    changes

--- naver.py
+++ naver.py
@@ -43,7 +43,7 @@ from itertools import product
 # driver = webdriver.Chrome(service=Service) # --> run this code once then deactiuvate it like shift + #
 options = webdriver.ChromeOptions() 
 options.add_argument('--disable-dev-shm-usage')
-# options.add_argument('--headless=new')
+options.add_argument('--headless=new')
 options.add_argument("--no-sandbox")
 options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
 options.add_argument("--window-size=1920x1080")
