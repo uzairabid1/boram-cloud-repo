db57827fca0783b364758d0fc8a6aa4992a92d1b
unknown <uzairabid222@gmail.com>
2024-03-20 23:00:17 +0500

    changes

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    python naver.py  
+    python3 naver.py  
 
     if [ -f "output_list5.csv" ]; then
         tail -n 1 output_list5.csv | sed -i '$ d' output_list5.csv
