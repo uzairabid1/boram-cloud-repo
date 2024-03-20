fc9950ffa049c32ffe0a9b3df2f60131c14893bc
unknown <uzairabid222@gmail.com>
2024-03-20 22:17:32 +0500

    changes

--- run_script.sh
+++ run_script.sh
@@ -3,11 +3,11 @@
 while true; do
     python naver.py  
 
-    if [ -f "output_list10.csv" ]; then
-        tail -n 1 output_list10.csv | sed -i '$ d' output_list10.csv
-        echo "Removed the last line from 'output_list10.csv'"
+    if [ -f "output_list2.csv" ]; then
+        tail -n 1 output_list2.csv | sed -i '$ d' output_list2.csv
+        echo "Removed the last line from 'output_list2.csv'"
     else
-        echo "'output_list10.csv' not found. Continuing without removing the last line."
+        echo "'output_list2.csv' not found. Continuing without removing the last line."
     fi
     
     echo "Script stopped. Restarting in 10 seconds..."
